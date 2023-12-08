7fa34a8d700cf8f0abe592be7a8338243b4e23d9
dangminhthong17 <152803492+dangminhthong17@users.noreply.github.com>
2023-12-08 17:38:49 +0700

    Add files via upload

--- src/main/java/com/tdtu/backend/repository/BookingRepository.java
+++ src/main/java/com/tdtu/backend/repository/BookingRepository.java
@@ -1,7 +1,11 @@
 package com.tdtu.backend.repository;
 
 import com.tdtu.backend.model.Booking;
+import com.tdtu.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findBookingsByUser(User user);
 }
